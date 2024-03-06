417a8c23a9f98d3e1b48d51c9c7c7997bef3d497
Breno Ingwersen <brenoingwersen@gmail.com>
2024-03-06 16:13:27 -0300

    fix(models): Change id columns from String to UUID and ajust the flights status to use the pre-defined list of possible values.

--- api/models/flights.py
+++ api/models/flights.py
@@ -1,23 +1,32 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
-from utils import generate_uuid
+from sqlalchemy import (Column,
+                        Integer,
+                        String,
+                        ForeignKey,
+                        DateTime,
+                        Boolean,
+                        Enum)
+from sqlalchemy.dialects.postgresql import UUID
 from typing import Dict
+from uuid import uuid4
+from schemas.flights import Status
 
 
 Base = declarative_base()
 
+
 class FlightModel(Base):
     __tablename__ = "flights"
 
-    flight_id = Column(String, primary_key=True, default=generate_uuid)
-    airline_id = Column(String, default=generate_uuid)
-    origin_airport_id = Column(String, default=generate_uuid)
-    destination_airport_id = Column(String, default=generate_uuid)
+    flight_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    airline_id = Column(UUID(as_uuid=True), default=uuid4)
+    origin_airport_id = Column(UUID(as_uuid=True), default=uuid4)
+    destination_airport_id = Column(UUID(as_uuid=True), default=uuid4)
     distance = Column(Integer, nullable=False)
     scheduled_departure = Column(DateTime, nullable=False)
     scheduled_arrival = Column(DateTime, nullable=False)
     arrival_delay = Column(Integer, nullable=False, default=0)
-    status = Column(String, nullable=False)
+    status = Column(Enum(Status), nullable=False)
     cancellation_reason = Column(String)
     air_system_delay = Column(Boolean, nullable=False)
     security_delay = Column(Boolean, nullable=False)
