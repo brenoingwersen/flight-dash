122fb649a7879e896333cab2f5b3ef606b9c7eff
Breno Ingwersen <brenoingwersen@gmail.com>
2024-03-09 14:20:17 -0300

    feat(api): Add airlines db model

---
+++ api/models/airlines.py
@@ -0,0 +1,20 @@
+from models.base import Base
+from sqlalchemy import (Column,
+                        String)
+from sqlalchemy.dialects.postgresql import UUID
+from typing import Dict
+from uuid import uuid4
+
+
+class AirlineModel(Base):
+    __tablename__ = "airlines"
+
+    airline_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    iata_code = Column(String, nullable=False)
+    airline = Column(String, nullable=False)
+
+    def dict(self) -> Dict:
+        """
+        Returns a dictionay representation of an airline.
+        """
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
