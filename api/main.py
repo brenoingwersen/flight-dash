50e0815518c1700249f4ff71fa785ce26d75d9b8
Breno Ingwersen <brenoingwersen@gmail.com>
2024-03-09 19:31:54 -0300

    feat(api): Add airlines routes to API and adjust tags names.

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.flights import router as flights_router
+from routes.airlines import router as airlines_router
 
 
 # Instantiate the app
 app = FastAPI()
 
 # Add the routes
-app.include_router(flights_router, prefix="/flights")
+app.include_router(flights_router, prefix="/flights", tags=["flights"])
+app.include_router(airlines_router, prefix="/airlines", tags=["airlines"])
