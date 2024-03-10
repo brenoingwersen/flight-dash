e9bbe0efd8fe9b3296e14f3c00f3e6605e675ba4
Breno Ingwersen <brenoingwersen@gmail.com>
2024-03-10 20:38:41 -0300

    fix(api): Change routes to handle the session dependency.

--- api/routes/airlines.py
+++ api/routes/airlines.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from starlette import status
-from repository.unit_of_work import UnitOfWork
 from repository.airlines import AirlinesRepository
 from sqlalchemy.exc import NoResultFound
 from fastapi.exceptions import HTTPException
 from fastapi import Response
+from deps import SessionDep
 
 # Schemas
 from schemas.airlines import (CreateAirlineSchema,
@@ -20,20 +20,21 @@ router = APIRouter()
 
 
 @router.get("/", response_model=GetAirlinesSchema)
-async def get_airlines(limit: Optional[int]) -> Dict[str, List]:
+async def get_airlines(session: SessionDep,
+                       limit: Optional[int]) -> Dict[str, List]:
     """
     GET endpoint for retrieving airlines.
     """
-    with UnitOfWork() as unit_of_work:
-        airlines_repo = AirlinesRepository(unit_of_work.session)
-        airlines = airlines_repo.list(limit)
+    airlines_repo = AirlinesRepository(session)
+    airlines = airlines_repo.list(limit)
     return {"airlines": airlines}
 
 
-@router.post("/", 
+@router.post("/",
              status_code=status.HTTP_201_CREATED,
              response_model=GetAirlineSchema)
-async def post_airline(payload: CreateAirlineSchema) -> Dict:
+async def post_airline(session: SessionDep,
+                       payload: CreateAirlineSchema) -> Dict:
     """
     POST endpoint for creating a new airline.
     """
@@ -41,61 +42,62 @@ async def post_airline(payload: CreateAirlineSchema) -> Dict:
     airline_payload = payload.model_dump()
     # Create a new id and add to tb
     airline_payload.update({"airline_id": uuid4()})
-    with UnitOfWork() as unit_of_work:
-        airlines_repo = AirlinesRepository(unit_of_work.session)
-        new_airline = airlines_repo.create(airline_payload)
-        unit_of_work.commit()
+    airlines_repo = AirlinesRepository(session)
+    new_airline = airlines_repo.create(airline_payload)
+    session.commit()
     return new_airline
 
 
 @router.get("/{airline_id}", response_model=GetAirlinesSchema)
-async def get_airline(airline_id: UUID) -> Dict:
+async def get_airline(session: SessionDep,
+                      airline_id: UUID) -> Dict:
     """
     GET endpoint for retrieving an airline.
     """
+    airlines_repo = AirlinesRepository(session)
     try:
-        with UnitOfWork() as unit_of_work:
-            airlines_repo = AirlinesRepository(unit_of_work.session)
-            airline = airlines_repo.get(airline_id)
+        airline = airlines_repo.get(airline_id)
         return airline
     except NoResultFound:
-        raise HTTPException(status_code=404, 
-                             detail=f"airline with id {airline_id} not found.")
+        raise HTTPException(status_code=404,
+                            detail=f"airline with id {airline_id} not found.")
 
 
-@router.put("/{airline_id}", 
+@router.put("/{airline_id}",
             status_code=status.HTTP_201_CREATED,
             response_model=GetAirlineSchema)
-async def update_airline(airline_id: UUID, 
-                   payload: CreateAirlineSchema) -> Dict:
+async def update_airline(session: SessionDep,
+                         airline_id: UUID,
+                         payload: CreateAirlineSchema) -> Dict:
     """
     PUT endpoint for updating an airline.
     """
-    airline_payload = payload.model_dump()  
-    try:  
-        with UnitOfWork() as unit_of_work:
-            airlines_repo = AirlinesRepository(unit_of_work.session)
-            updated_airline = airlines_repo.update(airline_id=airline_id, 
-                                                payload=airline_payload)
-            unit_of_work.commit()
+    airline_payload = payload.model_dump()
+    airlines_repo = AirlinesRepository(session)
+    try:
+            
+        updated_airline = airlines_repo.update(airline_id=airline_id,
+                                               payload=airline_payload)
+        session.commit()
         return updated_airline
     except NoResultFound:
-        raise HTTPException(status_code=404, 
-                             detail=f"airline with id {airline_id} not found.")
-    
-@router.delete("/{airline_id}", 
-            status_code=status.HTTP_204_NO_CONTENT,
-            response_class=Response)
-async def delete_airline(airline_id: UUID) -> Response:
+        raise HTTPException(status_code=404,
+                            detail=f"airline with id {airline_id} not found.")
+
+
+@router.delete("/{airline_id}",
+               status_code=status.HTTP_204_NO_CONTENT,
+               response_class=Response)
+async def delete_airline(session: SessionDep,
+                         airline_id: UUID) -> Response:
     """
     DELETE endpoint for deleting an airline.
     """
-    try:  
-        with UnitOfWork() as unit_of_work:
-            airlines_repo = AirlinesRepository(unit_of_work.session)
-            airlines_repo.delete(airline_id)
-            unit_of_work.commit()
+    airlines_repo = AirlinesRepository(session)
+    try:
+        airlines_repo.delete(airline_id)
+        session.commit()
         return Response(status_code=204)
     except NoResultFound:
-        raise HTTPException(status_code=404, 
-                             detail=f"airline with id {airline_id} not found.")
+        raise HTTPException(status_code=404,
+                            detail=f"airline with id {airline_id} not found.")
--- api/routes/flights.py
+++ api/routes/flights.py
@@ -1,11 +1,10 @@
 # Main
-from fastapi import APIRouter, Depends, HTTPException
-from starlette.requests import Request
+from fastapi import APIRouter, HTTPException
 from starlette.responses import Response
 from starlette import status
 from fastapi import HTTPException
 from repository.flights import FlightsRepository
-from repository.unit_of_work import UnitOfWork
+from deps import SessionDep
 
 # Schcmeas
 from schemas.flights import (GetFlightSchema,
@@ -21,21 +20,22 @@ from sqlalchemy.exc import NoResultFound
 router = APIRouter()
 
 @router.get("/", response_model=GetFlightsSchema)
-async def get_flights(limit: Optional[int] = None) -> Dict[str, List]:
+async def get_flights(session: SessionDep, 
+                      limit: Optional[int] = None) -> Dict[str, List]:
     """
     Endpoint to list all flights.
     """
-    with UnitOfWork() as unit_of_work:
-        # Dependency injection
-        flights_repo = FlightsRepository(unit_of_work.session)
-        flights = flights_repo.list(limit)
+    # Dependency injection
+    flights_repo = FlightsRepository(session)
+    flights = flights_repo.list(limit)
     return {"flights": flights}
 
 
 @router.post("/",
              status_code=status.HTTP_201_CREATED,
              response_model=GetFlightSchema)
-async def create_flight(payload: CreateFlightSchema) -> Dict:
+async def create_flight(session: SessionDep,
+                        payload: CreateFlightSchema) -> Dict:
     """
     Endpoint to create a new flight.
     """
@@ -43,22 +43,21 @@ async def create_flight(payload: CreateFlightSchema) -> Dict:
     flight_payload = payload.model_dump()
     # Add a flight_id
     flight_payload.update({"flight_id": uuid4()})
-    with UnitOfWork() as unit_of_work:
-        flight_repo = FlightsRepository(unit_of_work.session)
-        new_flight = flight_repo.create(flight_payload)
-        unit_of_work.commit()
+    flight_repo = FlightsRepository(session)
+    new_flight = flight_repo.create(flight_payload)
+    session.commit()
     return new_flight
 
 
 @router.get("/{flight_id}", response_model=GetFlightSchema)
-async def get_flight(flight_id: UUID):
+async def get_flight(session: SessionDep, 
+                     flight_id: UUID):
     """
     Endpoint to get a flight.
     """
+    flights_repo = FlightsRepository(session)
     try:
-        with UnitOfWork() as unit_of_work:
-            flights_repo = FlightsRepository(unit_of_work.session)
-            flight = flights_repo.get(flight_id)
+        flight = flights_repo.get(flight_id)
         return flight
     except NoResultFound:
         raise HTTPException(status_code=404,
@@ -68,17 +67,17 @@ async def get_flight(flight_id: UUID):
 @router.put("/{flight_id}",
             status_code=status.HTTP_201_CREATED,
             response_model=GetFlightSchema)
-async def update_flight(flight_id: UUID,
+async def update_flight(session: SessionDep, 
+                        flight_id: UUID,
                         payload: CreateFlightSchema) -> Dict:
     """
     Endpoint for updating a flight.
     """
     flight_payload = payload.model_dump()
+    flights_repo = FlightsRepository(session)
     try:
-        with UnitOfWork() as unit_of_work:
-            flights_repo = FlightsRepository(unit_of_work.session)
-            flight = flights_repo.update(flight_id, flight_payload)
-            unit_of_work.commit() # Persists the changes to the db
+        flight = flights_repo.update(flight_id, flight_payload)
+        session.commit() # Persists the changes to the db
         return flight
     except NoResultFound:
         raise HTTPException(status_code=404,
@@ -88,15 +87,15 @@ async def update_flight(flight_id: UUID,
 @router.delete("/{flight_id}",
                status_code=status.HTTP_204_NO_CONTENT,
                response_class=Response)
-async def delete_flight(flight_id: UUID) -> Response:
+async def delete_flight(session: SessionDep, 
+                        flight_id: UUID) -> Response:
     """
     Endpoint for deleting a flight.
     """
-    try:
-        with UnitOfWork() as unit_of_work:
-            flights_repo = FlightsRepository(unit_of_work.session)
-            flights_repo.delete(flight_id)    
-            unit_of_work.commit() # Persists the changes to the db
+    flights_repo = FlightsRepository(session)
+    try:    
+        flights_repo.delete(flight_id)    
+        session.commit() # Persists the changes to the db
         return Response(status_code=204)
     except NoResultFound:
         raise HTTPException(status_code=404,
