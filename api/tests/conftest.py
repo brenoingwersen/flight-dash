9efd2b5eec1a9b01d50e159f29e3840fffb5cb3e
Breno Ingwersen <brenoingwersen@gmail.com>
2024-03-12 11:48:08 -0300

    fix(tests): Add api to PYTHONPATH and add sample airline data for unit testing the airlines routes.

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append("/home/user/flight-dash/api")
+
+
 from typing import Dict, Generator
 import os
 import pytest
@@ -16,6 +20,7 @@ SQLALCHEMY_TEST_DB_URI = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_TEST_DB_URI, poolclass=NullPool)
 SessionLocal = sessionmaker(bind=engine)
 
+
 def get_test_db() -> Generator[Session, None, None]:
     """
     Dependency for generating sessions.
@@ -32,6 +37,7 @@ def get_test_db() -> Generator[Session, None, None]:
         # Always close the session
         session.close()
 
+
 # Inject engine dependency
 app.dependency_overrides[get_db] = get_test_db
 
@@ -42,31 +48,45 @@ def sample_flight_data() -> Dict:
     Returns a sample flight.
     """
     return {
-            "flight_id": "b0b697b5-f8c8-4901-9812-b1092bbb8881",
-            "airline_id": "0a182982-e0ec-44d2-9c8a-fdb01c7a878c",
-            "origin_airport_id": "2ca1504e-03bc-489d-a371-28861ce1769f",
-            "destination_airport_id": "91a587e1-58f1-4281-ba34-5270f66db0ca",
-            "distance": 0,
-            "scheduled_departure": "2024-01-01T00:00:00.0Z",
-            "scheduled_arrival": "2024-01-01T03:00:00.0Z",
-            "arrival_delay": 0,
-            "status": "landed",
-            "cancellation_reason": "A",
-            "air_system_delay": True,
-            "security_delay": False,
-            "airline_delay": False,
-            "late_aircraft_delay": True,
-            "weather_delay": False
+        "flight_id": "b0b697b5-f8c8-4901-9812-b1092bbb8881",
+        "airline_id": "0a182982-e0ec-44d2-9c8a-fdb01c7a878c",
+        "origin_airport_id": "2ca1504e-03bc-489d-a371-28861ce1769f",
+        "destination_airport_id": "91a587e1-58f1-4281-ba34-5270f66db0ca",
+        "distance": 0,
+        "scheduled_departure": "2024-01-01T00:00:00.0Z",
+        "scheduled_arrival": "2024-01-01T03:00:00.0Z",
+        "arrival_delay": 0,
+        "status": "landed",
+        "cancellation_reason": "A",
+        "air_system_delay": True,
+        "security_delay": False,
+        "airline_delay": False,
+        "late_aircraft_delay": True,
+        "weather_delay": False
     }
 
 
 @pytest.fixture(scope="function")
-def test_db(sample_flight_data):
-    init_test_db(SQLALCHEMY_TEST_DB_URI,
-                 [sample_flight_data])
+def sample_airline_data() -> Dict:
+    """
+    Returns a sample airline.
+    """
+    return {
+        "airline_id": "0a182982-e0ec-44d2-9c8a-fdb01c7a878c",
+        "iata_code": "AA",
+        "airline": "Test Airline Co."
+    }
 
-    yield 
-    
+
+@pytest.fixture(scope="function")
+def test_db(sample_flight_data,
+            sample_airline_data):
+    init_test_db(SQLALCHEMY_TEST_DB_URI,
+                 [sample_flight_data],
+                 [sample_airline_data])
+
+    yield
+
     # Drop the database after the test function completes
     os.remove("./test.db")
 
--- api/tests/init_test_db.py
+++ api/tests/init_test_db.py
@@ -4,36 +4,37 @@ from sqlalchemy.orm import sessionmaker
 
 # Models
 from models.flights import FlightModel
+from models.airlines import AirlineModel
+from models.base import Base
 
 # Schemas
 from schemas.flights import GetFlightSchema
-
-# Utils
-from uuid import UUID
-from datetime import datetime
-
+from schemas.airlines import GetAirlineSchema
 
 
 def init_test_db(db_url: str,
-                 sample_flight_data: List[Dict]):
+                 sample_flight_data: List[Dict],
+                 sample_airline_data: List[Dict]):
     """
     Creates a test database with the provided data.
     """
-    if not isinstance(sample_flight_data, list):
-        raise TypeError("sample_flight_data must be a list")
     engine = create_engine(db_url, echo=True)
 
-    FlightModel.metadata.create_all(bind=engine)
+    # Create tables
+    Base.metadata.create_all(bind=engine)
 
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     session = SessionLocal()
 
     # Validate the data
     valid_flight_data = [GetFlightSchema.model_validate(flight).model_dump() for flight in sample_flight_data]
+    valid_airline_data = [GetAirlineSchema.model_validate(airline).model_dump() for airline in sample_airline_data]
 
     # Inserting sample data
     flights = [FlightModel(**flight) for flight in valid_flight_data]
+    airlines = [AirlineModel(**airline) for airline in valid_airline_data]
     session.bulk_save_objects(flights)
+    session.bulk_save_objects(airlines)
     session.commit()
     session.close()
 
